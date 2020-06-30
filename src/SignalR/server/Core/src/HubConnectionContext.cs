32243f19e47ef098329e21aeda4632138d33aa67
BrennanConroy <brecon@microsoft.com>
2020-06-29 17:17:48 -0700

    stash

--- src/SignalR/server/Core/src/HubConnectionContext.cs
+++ src/SignalR/server/Core/src/HubConnectionContext.cs
@@ -73,6 +73,9 @@ namespace Microsoft.AspNetCore.SignalR
 
             _systemClock = contextOptions.SystemClock ?? new SystemClock();
             _lastSendTimeStamp = _systemClock.UtcNowTicks;
+
+            var maxInvokes = contextOptions.MaximumParallelInvocations;
+            ActiveInvocationLimit = new SemaphoreSlim(maxInvokes - 1, maxInvokes);
         }
 
         internal StreamTracker StreamTracker
@@ -92,6 +95,8 @@ namespace Microsoft.AspNetCore.SignalR
         internal HubCallerContext HubCallerContext { get; }
 
         internal List<Task> ActiveHubInvocations { get; } = new List<Task>();
+
+        internal SemaphoreSlim ActiveInvocationLimit { get; }
 
         internal Exception CloseException { get; private set; }
 
