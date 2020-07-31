036da4b41e6282075a90f85c4715aa20b3692505
Steve Sanderson <SteveSandersonMS@users.noreply.github.com>
2020-07-31 16:11:52 +0100

    Begin implementing Append methods in RenderTreeArrayBuilder

--- src/Components/Components/src/Rendering/RenderTreeBuilder.cs
+++ src/Components/Components/src/Rendering/RenderTreeBuilder.cs
@@ -55,7 +55,7 @@ namespace Microsoft.AspNetCore.Components.Rendering
             }
 
             _openElementIndices.Push(_entries.Count);
-            _entries.Append(RenderTreeFrame.Element(sequence, elementName));
+            _entries.AppendElement(sequence, elementName);
             _lastNonAttributeFrameType = RenderTreeFrameType.Element;
             ProfilingEnd();
         }
@@ -88,7 +88,7 @@ namespace Microsoft.AspNetCore.Components.Rendering
         public void AddMarkupContent(int sequence, string? markupContent)
         {
             ProfilingStart();
-            _entries.Append(RenderTreeFrame.Markup(sequence, markupContent ?? string.Empty));
+            _entries.AppendMarkup(sequence, markupContent ?? string.Empty);
             _lastNonAttributeFrameType = RenderTreeFrameType.Markup;
             ProfilingEnd();
         }
